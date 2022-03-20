5615e376706b57bfe2993e6a3ea5f49c9f5f1f32
Tom Hunter <tom@markerr.com>
2022-03-20 08:59:47 -0400

    add more tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,10 @@
+
 from pathlib import Path
+from typing import Tuple
 
 import pytest
+
+TEST_ROOT = "tests/output"
 
 
 @pytest.fixture
@@ -26,21 +30,41 @@ def expected_input():
 @pytest.fixture
 def actual_input():
     from johnny_decimal.core import Input
-    return Input('tests/index.toml')
+    return Input('tests/data/index.toml')
 
-@pytest.fixture
-def expected_area_id():
-    return 123
-
-@pytest.fixture
-def expected_area_path():
-    return 'tests/'
-
-@pytest.fixture
-def expected_area_root():
-    return Path('.')
+## actual area & categories
 
 @pytest.fixture
 def actual_area():
     from johnny_decimal.core import Area
     return Area('test-area')
+
+@pytest.fixture
+def actual_category():
+    from johnny_decimal.core import Area, Category
+    area = Area('test-area')
+    return Category(name='test-category', area=area)
+
+## actual registry object
+
+@pytest.fixture
+def actual_area_registry():
+    from johnny_decimal.core import Area, Category, Registry
+    areas = [
+        Area('test-area-1', root=Path(TEST_ROOT)),
+        Area('test-area-2', root=Path(TEST_ROOT)),
+    ]
+    return Registry(areas=areas)
+
+@pytest.fixture
+def actual_category_registry():
+    from johnny_decimal.core import Area, Category, Registry
+    areas = [
+        Area('test-area-1', root=Path(TEST_ROOT)),
+        Area('test-area-2', root=Path(TEST_ROOT)),
+    ]
+    categories = [
+        Category(name='test-category-1', area=areas[0]),
+        Category(name='test-category-2', area=areas[1]),
+    ]
+    return Registry(categories=categories)
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,9 +1,19 @@
 import pdb
+import shutil
 from pathlib import Path
 
 import pytest
 
 from johnny_decimal.core import Area, Category, Input, Registry
+from johnny_decimal.exceptions import AttributeNotDefined
+
+TEST_ROOT = "tests/output"
+
+@pytest.fixture(scope='module')
+def file_system():
+    yield file_system
+    print('******TEARDOWN******')
+    shutil.rmtree(TEST_ROOT)
 
 
 class TestInput:
@@ -22,42 +32,159 @@ class TestArea:
     Tests for Area class
     """
 
-    # TODO: test for raising of AttributeNotDefined -- id/path attributes can only be set in a registry
-    # def test_path(self,
-    #               actual_area: Area,
-    #               expected_area_path: str):
-    #     actual = actual_area.path
-    #     assert actual == expected_area_path
-
-    # TODO: test for raising of AttributeNotDefined -- id/path attributes can only be set in a registry
-    # def test_id(self,
-    #               actual_area: Area,
-    #               expected_area_id: int):
-    #     breakpoint()
-    #     # actual = actual_area.id
-    #     assert actual == expected_area_id
-
     def test_root(self,
-                  actual_area: Area,
-                  expected_area_root: Path):
-        actual = actual_area.root
-        assert actual == expected_area_root
-
-    # TODO: test that area can create directory for given path
-    def test_mkdir(self,
                   actual_area: Area):
-                #   expected_area_obj: Area):
-        # actual = actual_area.path
-        # assert actual == expected_area
-        pass
+        actual = actual_area.root
+        assert actual == Path('.')
+
+    def test_id(self,
+                  actual_area: Area):
+        """
+        Test that id attributes is an empty string while defined outside of a registry
+        """
+        assert actual_area.id == ''
+
+    def test_path(self,
+                  actual_area: Area):
+        """
+        Test that path attributes should raise exception when trying to 
+        access outside of registry
+        """
+        with pytest.raises(AttributeNotDefined, match=r'[.]*id[.]*'):
+            actual_area.path
+
+    def test_mkdir(self,
+                  actual_area: Category):
+        """
+        Test that area object can't call mkdir because the ID attribute can only be defined
+        in a registry object.
+        """
+        with pytest.raises(AttributeNotDefined, match=r'[.]*id[.]*'):
+            actual_area.mkdir()
 
 class TestCategory:
     """
     Tests for Category class
     """
 
+    def test_id(self,
+                actual_category: Category):
+        """
+        Test that id attributes is an empty string while defined outside of a registry
+        """
+        assert actual_category.id == ''
+
+    def test_path(self,
+                  actual_category: Category):
+        """
+        Test that path attributes should raise exception when trying to 
+        access outside of registry
+        """
+        with pytest.raises(AttributeNotDefined, match=r'[.]*id[.]*'):
+            actual_category.path
+
+    def test_mkdir(self,
+                  actual_category: Category):
+        """
+        Test that category object can't call mkdir because the ID attribute can only be defined
+        in a registry object.
+        """
+        with pytest.raises(AttributeNotDefined, match=r'[.]*id[.]*'):
+            actual_category.mkdir()
+
 class TestReigstry:
     """
     Tests for Registry class
     """
 
+    def test__detect_input_style(self, 
+                                 actual_area_registry: Registry,
+                                 actual_category_registry: Registry):
+
+        assert actual_area_registry._detect_input_style() == 'areas'
+        assert actual_category_registry._detect_input_style() == 'categories'
+
+    def test_area_get_max_id_range(self, actual_area_registry: Registry):
+
+        actual1 = actual_area_registry.areas[0].get_max_id_range()
+        assert actual1 == 19
+
+        actual2 = actual_area_registry.areas[1].get_max_id_range()
+        assert actual2 == 29
+
+    def test_area_get_max_id_range(self, actual_area_registry: Registry):
+
+        actual1 = actual_area_registry.areas[0].get_min_id_range()
+        assert actual1 == 10
+
+        actual2 = actual_area_registry.areas[1].get_min_id_range()
+        assert actual2 == 20
+
+    def test_area_id(self, actual_area_registry: Registry):
+        """
+        Test that id attributes for input areas are defined as expected
+        """
+        assert actual_area_registry.areas[0].id == '10-19'
+        assert actual_area_registry.areas[1].id == '20-29'
+
+    def test_area_path(self, actual_area_registry: Registry):
+        """
+        Test that path attributes for input areas are defined as expected
+        """
+        root = actual_area_registry.areas[0].root
+        assert actual_area_registry.areas[0].path == Path(f'{root}/10-19_test-area-1')
+        assert actual_area_registry.areas[1].path == Path(f'{root}/20-29_test-area-2')
+
+    def test_category_id(self, actual_category_registry: Registry):
+        """
+        Test that id attributes for input categories are defined as expected
+        """
+        assert actual_category_registry.categories[0].id == '10'
+        assert actual_category_registry.categories[1].id == '21' #TODO: fix this error
+
+    def test_category_path(self, actual_category_registry: Registry):
+        """
+        Test that path attributes for input categories are defined as expected
+        """
+        root = actual_category_registry.categories[0].area.root
+        assert actual_category_registry.categories[0].path == Path(f'{root}/10-19_test-area-1/10_test-category-1')
+        assert actual_category_registry.categories[1].path == Path(f'{root}/20-29_test-area-2/21_test-category-2')
+
+    def test_area_mkdir(self, actual_area_registry: Registry, file_system):
+        """
+        Test that area object can call mkdir as expected
+        """
+        root = actual_area_registry.areas[0].root
+        actual_area_registry.areas[0].mkdir()
+        actual_area_registry.areas[1].mkdir()
+        output1 = root / '10-19_test-area-1'
+        output2 = root / '20-29_test-area-2'
+        assert output1.exists()
+        assert output2.exists()
+
+    def test_category_mkdir(self, actual_category_registry: Registry, file_system):
+        """
+        Test that category object can call mkdir as expected
+        """
+        root = actual_category_registry.categories[0].area.root
+        actual_category_registry.categories[0].mkdir()
+        actual_category_registry.categories[1].mkdir()
+        output1 = root / '10-19_test-area-1/10_test-category-1'
+        output2 = root / '20-29_test-area-2/21_test-category-2'
+        assert output1.exists()
+        assert output2.exists()
+
+    def test_init(self, 
+                  actual_area_registry: Registry, 
+                  actual_category_registry: Registry, 
+                  file_system):
+        """
+        Test that area object can call mkdir as expected
+        """
+        root = actual_area_registry.areas[0].root
+        actual_area_registry.init()
+        actual_category_registry.init()
+        output1 = root / '10-19_test-area-1/10_test-category-1'
+        output2 = root / '20-29_test-area-2/21_test-category-2'
+        assert output1.exists()
+        assert output2.exists()
